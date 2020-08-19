7c43bb595eb13d0928f28a43636d14d36297ea9a
Jordina Gavalda <jordinagavalda@gmail.com>
2020-08-19 21:06:14 +0200

    ended easy version

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -15,17 +15,14 @@ def full_board():
     else:
         print("It's a tie")
         quit()
-        end_game()
 
 def check_win():
     if (board[1] == 'x' and board[2] == 'x' and board[3] == 'x') or (board[4] == 'x' and board[5] == 'x' and board[6] == 'x') or (board[7] == 'x' and board[8] == 'x' and board[9] == 'x') or (board[1] == 'x' and board[4] == 'x' and board[7] == 'x') or (board[2] == 'x' and board[5] == 'x' and board[8] == 'x') or (board[3] == 'x' and board[6] == 'x' and board[9] == 'x') or (board[1] == 'x' and board[5] == 'x' and board[9] == 'x') or (board[3] == 'x' and board[5] == 'x' and board[7] == 'x'):
         print('You win! congrats')
         quit()
-        end_game()
     elif (board[1] == 'o' and board[2] == 'o' and board[3] == 'o') or (board[4] == 'o' and board[5] == 'o' and board[6] == 'o') or (board[7] == 'o' and board[8] == 'o' and board[9] == 'o') or (board[1] == 'o' and board[4] == 'o' and board[7] == 'o') or (board[2] == 'o' and board[5] == 'o' and board[8] == 'o') or (board[3] == 'o' and board[6] == 'o' and board[9] == 'o') or (board[1] == 'o' and board[5] == 'o' and board[9] == 'o') or (board[3] == 'o' and board[5] == 'o' and board[7] == 'o'):
         print('The computer won')
         quit()
-        end_game()
     else:
         return
 
@@ -38,21 +35,19 @@ def player_move(move):
         print('Invalid move')
         main()
 
-
 def pc_move(move):
-    move = 3
+    move = random.randrange(1, 10)
     if board[move] == ' ':
         board[move] =  'o'
         return move
     else:
-        move = 4
-        board[move] = 'o'
-        return move
+        pc_move(move)
 
-def end_game():
+def ai(move):
     pass
 
 def main():
+
     check_win()
     full_board()
     move = int(input("A quina posició tires? 1-9"))
@@ -65,4 +60,32 @@ def main():
 
     main()
 
-main()
+def main2():
+
+    check_win()
+    full_board()
+    move = int(input("A quina posició tires? 1-9   "))
+    player_move(move)
+    print_board(board)
+    print("\n")
+    ai(move)
+    print_board(board)
+    print("\n")
+
+    main2()
+
+
+def version():
+    version = str(input("Vols jugar a la versió fàcil o a la difícil? (f/d)"))
+
+    if version.lower() == 'f':
+        print('fàcil')
+        main()
+    elif version.lower() == 'd':
+        print('dificil')
+        main2()
+    else:
+        print('Invàlid')
+        version()
+
+version()
